9188f45369974179ed0751f81656d07521d84619
zhuohengYing <zoranying@gmail.com>
2026-07-17 19:04:11 +0800

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,10 +1,10 @@
-/*
+
   Optional profile links.
   Leave a value empty to hide that link.
   GitHub is inferred automatically when the site is hosted at username.github.io.
 */
 window.SITE_CONFIG = {
-  github: "",
-  scholar: "",
-  linkedin: ""
+  github: "https://github.com/zoranying-sudo/yingzhuoheng",
+  scholar: "https://scholar.google.com",
+  linkedin: "https://www.linkedin.com"
 };
